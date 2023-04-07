36dee009ec0a63b5f6b8f36204e6517f2407ba38
Haley Dawe <hdawe@my.bcit.ca>
2023-04-07 15:44:47 -0700

    updated Me.jsx

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-bold">Hello, I'm <span className="text-blue-500">Haley</span></h1>
+        <h1 className="text-6xl font-bold">Hello, Im <span className="text-blue-500">Haley</span></h1>
         </div>
     </main>
   )
